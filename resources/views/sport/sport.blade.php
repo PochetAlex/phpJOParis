161143e6ded1b213870a407cefc00d69c62a560c
alex.pochet <alex_pochet@ens.univ-artois.fr>
2023-11-08 11:00:43 +0100

    ajout affichage web

--- resources/views/sport/sport.blade.php
+++ resources/views/sport/sport.blade.php
@@ -1,17 +1,53 @@
 <html>
 <head>
-    <title>Liste des tâches</title>
+    <title>Liste des sports</title>
+    <style>
+        table, td {
+            border: 1px solid;
+            border-collapse: collapse;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        tr, td, body {
+            text-align: center;
+            padding: 5px;
+        }
+    </style>
 </head>
 <body>
-<h2>La liste des tâches</h2>
+    <h2>La liste des tâches</h2>
 
 @if(!empty($sports))
-    <ul>
-        @foreach($sports as $sport)
-            <li>{{$sport['nom']}} {{$sport['description']}}
-            </li>
-        @endforeach
-    </ul>
+        <table>
+            <thead>
+            <tr>
+                <td>ID</td>
+                <td>Nom</td>
+                <td>Description</td>
+                <td>Année d'ajout</td>
+                <td>Nombre de disciplines</td>
+                <td>Nombre d'épreuves</td>
+                <td>Date de debut</td>
+                <td>Date de fin</td>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($sports as $sport)
+                <tr>
+                    <td>{{$sport['id']}}</td>
+                    <td>{{$sport['nom']}}</td>
+                    <td>{{$sport['description']}}</td>
+                    <td>{{$sport['annee_ajout']}}</td>
+                    <td>{{$sport['nb_disciplines']}}</td>
+                    <td>{{$sport['nb_epreuves']}}</td>
+                    <td>{{$sport['date_debut']}}</td>
+                    <td>{{$sport['date_fin']}}</td>
+                </tr>
+
+            @endforeach
+            </tbody>
+        </table>
+
 @else
     <h3>aucun sport</h3>
 @endif
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Sport;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $sports = Sport::all();
+    return view('sport/sport', ['sports' => $sports]);
 });
 
 Route::get('sport', [App\Http\Controllers\SportController::class,'index']) -> name('sport.index');
