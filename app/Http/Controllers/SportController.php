fc5029b162be719283fc35c6e595afe78dfa8901
alex_pochet <alex_pochet@ens.univ-artois.fr>
2024-01-10 17:26:20 +0100

    :bug: correction de la route dans SportController

--- app/Http/Controllers/SportController.php
+++ app/Http/Controllers/SportController.php
@@ -9,6 +9,6 @@ class SportController extends Controller
 {
     public function index() {
         $sports = Sport::all(); // stocke dans la variable $Taches, les objets Tache récupérés dans la table taches de la base de données.
-        return view('sports.index', ['sports' => $sports]);
+        return view('sport/sport', ['sports' => $sports]);
     }
 }
